ec371cb0baae6b47d1ea0e7f11e04301e9a5d03e
machi1m <90927291+machi1m@users.noreply.github.com>
2026-02-04 09:06:37 -0600

    If, for, short circuiting

---
+++ src/main/java/org/example/ForClass.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ForClass {
+
+    public static void main(String[] args) {
+        System.out.println("Nikhitha");
+        System.out.println("Nikhitha");
+
+        for(int i = 0 ; i < 10; i++ ){
+            System.out.println("Nikhitha");
+        }
+
+        int i = 1000;
+
+        while(networkResponse.isNotReturned()){
+            call the website;
+        }
+
+    }
+
+}
